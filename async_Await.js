9ad9de2844306a58b4816b2959be8d867f1f31c1
Praksh671 <prakashsochandrasekar@gmail.com>
2024-12-02 17:09:53 +0530

    Async Await practice

---
+++ async_Await.js
@@ -0,0 +1,40 @@
+// basic async
+async function aysFunction() {
+  return "Asynchronous function sample";
+}
+
+aysFunction().then((data) => {
+  console.log(data);
+});
+
+// async await
+
+function asynchronousFunction() {
+  let promise1 = Promise.resolve("promise 1 is resolved successfully");
+  let promise2 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("this method is 1s delay due to timeout");
+    }, 1000);
+  });
+
+  let combinedPromises = Promise.all([promise1, promise2]);
+  return combinedPromises;
+}
+
+console.log(asynchronousFunction());
+
+function display() {
+  let synchornousFrunction = asynchronousFunction();
+  console.log("inside Display without async await.....");
+
+  console.log(synchornousFrunction);
+}
+
+async function asyncAwaitFn() {
+  let awaitFunc = await asynchronousFunction();
+  console.log("inside Display async await.....");
+  console.log(awaitFunc);
+}
+
+display();
+asyncAwaitFn();
