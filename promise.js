5cf77cdb3563a28db5ca7ecb788a5dbc98795227
Praksh671 <prakashsochandrasekar@gmail.com>
2024-12-02 16:23:50 +0530

    Promises concept

--- promise.js
+++ promise.js
@@ -4,7 +4,7 @@ const promise = new Promise((resolve, reject) => {
     resolve("User exists in the data");
   } else {
     //if isUserExists is false, nothing happens when there’s no reject. The catch block will never be executed.
-    reject("Error not found");
+    reject("Error not promise");
   }
 });
 
@@ -13,8 +13,7 @@ const chancePromise = new Promise((resolve, reject) => {
   if (isChanceExists) {
     resolve("chance exists for user");
   } else {
-    //if isUserExists is false, nothing happens when there’s no reject. The catch block will never be executed.
-    reject("Error not found");
+    reject("Error not chancePromise");
   }
 });
 
@@ -23,8 +22,7 @@ const luckPromise = new Promise((resolve, reject) => {
   if (ispreiodExists) {
     resolve("luck exists for this User");
   } else {
-    //if isUserExists is false, nothing happens when there’s no reject. The catch block will never be executed.
-    reject("Error not found");
+    reject("Error not luckPromise");
   }
 });
 
@@ -41,4 +39,43 @@ promise
   )
   .catch((error) => {
     console.log(error);
+  });
+
+
+const promiseReject = new Promise((resolve, reject) => {
+  reject("promise rejected");
+});
+
+// promise all is typically used like completeable futures where it waits for all to get completed
+
+/* if this reject, promise ALL method will fail because this wants to be executed without failure should be success, 
+however race wont fail because this not the first method that is being executed
+*/
+
+Promise.all([promise, chancePromise, luckPromise, promiseReject]).then(
+  (promises) => {
+    console.log("---------- Promises ALL---------");
+    promises.forEach((prom, i) => {
+      i++;
+      console.log(i + "." + prom);
+    });
+  },
+  (err) => {
+    console.log("---------- Promises ALL---------");
+    console.log(err);
+  }
+);
+
+const promise1 = new Promise((resolve, reject) => {
+  resolve("promise is resolved successfully");
+});
+
+Promise.race([promise, promise1, promiseReject])
+  .then((promises) => {
+    console.log("---------- Promises Race---------");
+    console.log(promises);
+  })
+  .catch((err) => {
+    console.log("---------- Promises Race---------");
+    console.log(err);
   });
